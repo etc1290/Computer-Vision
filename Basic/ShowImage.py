f49b5940a1f3caeb2f11a3595a91855f18560248
etc1290 <52283388+etc1290@users.noreply.github.com>
2020-09-23 11:02:00 +0800

    Multi-Image

--- Basic/ShowImage.py
+++ Basic/ShowImage.py
@@ -10,8 +10,17 @@ import numpy as np
 
 
 img = cv2.imread('../img/Snivy.jpg')
+img2 = cv2.imread('../img/Minccino.jpg')
+
+img  = cv2.resize(img,(400,400))
+img2 = cv2.resize(img2,(400,400))
+
+numpy_horizontal = np.hstack((img, img2))
+numpy_horizontal_concat = np.concatenate((img,img2),axis=1)
+
+cv2.resizeWindow("enhanced", 400, 800);
 cv2.namedWindow('Image', cv2.WINDOW_NORMAL)
-cv2.imshow('Image',img)
+cv2.imshow('Image',numpy_horizontal_concat)
 
 #按下 "0" 關閉所有視窗
 cv2.waitKey(0)
